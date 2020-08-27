e8ca7c035104e7c460c25e9b921bafbcb8508519
Brandon <sorebran@isu.edu>
2020-08-27 16:09:28 -0600

    changed slashes to prevent being read as escape characters.

--- environments/vsphere_prod/tf_concourse/main.tf
+++ environments/vsphere_prod/tf_concourse/main.tf
@@ -43,6 +43,6 @@ module "vm_provision" {
   conn_type       = "ssh"
   ssh_user        = "svc-tf-dev"
   host            = module.concourse-ci.Linux-guest-ip
-  ssh_key         = "C:\Users\xiguazhi\.ssh\id_rsa"
+  ssh_key         = "C:/Users/xiguazhi/.ssh/id_rsa"
   bootstrap_name  = "bootstrap.sh"
 }
