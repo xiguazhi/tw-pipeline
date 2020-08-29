ace859b71ecb3103fc3fb914c2915f2e6a68a372
Brandon <sorebran@isu.edu>
2020-08-28 19:31:59 -0600

    removed extra lines.

--- modules/vsphere_linux_vm/main.tf
+++ modules/vsphere_linux_vm/main.tf
@@ -66,7 +66,6 @@ resource "vsphere_virtual_machine" "Linux" {
   count      = var.is_windows_image != "true" ? var.instances : 0
   depends_on = [var.vm_depends_on]
   name       = "${var.dc_abreviation}${var.environment}-${var.vmname}${count.index + 1}"
-  "${var.dc_abreviation}${var.environment}-${var.vmname}${count.index + 1}}"
   resource_pool_id  = data.vsphere_resource_pool.pool.id
   folder            = var.vmfolder
   tags              = var.tag_ids != null ? var.tag_ids : data.vsphere_tag.tag[*].id
